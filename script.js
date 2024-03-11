437f306bb5bf17f5f2d985182941825a17b1c484
Siddharth Ruria <siddharthruria10@gmail.com>
2024-03-12 01:07:31 +0530

    complete mouse hover function on states

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// ""
+// ""hg
 // hg
-// var map = L.map("india-map", {
-//   dragging: true,
-// });
 
 document.addEventListener("DOMContentLoaded", function () {
   var mapObject = document.getElementById("india-map");
@@ -12,20 +9,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
     var all = svgDoc.querySelectorAll(".states path");
 
+    const popup = document.getElementById("popup");
+
     all.forEach(function (state) {
       state.addEventListener("click", function () {
-        var pathName = state.getAttribute("name");
-        alert("you clicked on " + pathName);
+        var stateName = state.getAttribute("name");
+        alert("you clicked on " + stateName);
       });
 
       const originalColor = state.getAttribute("fill");
 
-      state.addEventListener("mouseenter", function () {
+      state.addEventListener("mouseover", function (event) {
+        this.style.cursor = "pointer";
         state.setAttribute("fill", "white");
+
+        ////////////////////////////////////// working on state-name-popup //////////////////////////////////////
+
+        const state_name = state.getAttribute("name");
+        popup.innerText = state_name;
+        popup.style.display = "block";
+
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+        popup.style.left = mouseX + 350 + "px";
+        popup.style.top = mouseY + 20 + "px";
       });
 
-      state.addEventListener("mouseleave", function () {
+      state.addEventListener("mouseout", function () {
+        this.style.cursor = "default";
         this.setAttribute("fill", originalColor);
+        popup = document.getElementById("popup");
+        popup.style.display = "none";
       });
     });
   });

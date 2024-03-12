4511efda2c0f96473f58afab51564849bb311070
Siddharth Ruria <siddharthruria10@gmail.com>
2024-03-12 11:26:44 +0530

    fixed state hover function

--- script.js
+++ script.js
@@ -28,18 +28,19 @@ document.addEventListener("DOMContentLoaded", function () {
         const state_name = state.getAttribute("name");
         popup.innerText = state_name;
         popup.style.display = "block";
-
-        const mouseX = event.clientX;
-        const mouseY = event.clientY;
-        popup.style.left = mouseX + 350 + "px";
-        popup.style.top = mouseY + 20 + "px";
       });
 
       state.addEventListener("mouseout", function () {
         this.style.cursor = "default";
         this.setAttribute("fill", originalColor);
-        popup = document.getElementById("popup");
         popup.style.display = "none";
+      });
+
+      state.addEventListener("mousemove", function (e) {
+        const mouseX = e.clientX;
+        const mouseY = e.clientY;
+        popup.style.left = mouseX + 380 + "px";
+        popup.style.top = mouseY + 50 + "px";
       });
     });
   });
